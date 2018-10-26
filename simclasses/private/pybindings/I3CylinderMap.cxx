61f12f1c86915e2419b7ecf263db92e82340deb0
Andres Medina <andres.medina@icecube.wisc.edu>
2018-10-26 04:20:10 +0000

    I3CylinderMap stuff
    git-svn-id: http://code.icecube.wisc.edu/svn/highlander/trunk@170505 16731396-06f5-0310-8873-f7f720988828

--- simclasses/private/pybindings/I3CylinderMap.cxx
+++ simclasses/private/pybindings/I3CylinderMap.cxx
@@ -6,8 +6,8 @@ using boost::python::dataclass_suite;
 
 void register_I3CylinderMap()
 {
-  class_<I3CylinderMap,
-    boost::shared_ptr<I3CylinderMap>>("I3CylinderMap")
+  class_< I3CylinderMap , boost::shared_ptr<I3CylinderMap> >("I3CylinderMap")
     .def(dataclass_suite<I3CylinderMap>())
     ;
+  bp::implicitly_convertible<boost::shared_ptr<I3CylinderMap>, boost::shared_ptr<const I3CylinderMap> >();
 }
--- simclasses/public/simclasses/I3CylinderMap.h
+++ simclasses/public/simclasses/I3CylinderMap.h
@@ -1,5 +1,13 @@
+#ifndef I3CYLINEDERMAP_H_INCLUDED 
+#define I3CYLINDERMAP_H_INCLUDED
+
 #include <map>
 #include <icetray/OMKey.h>
-#include "clsim/shadow/I3ExtraGeometryItemCylinder.h"
+#include <clsim/shadow/I3ExtraGeometryItemCylinder.h>
 
-using I3CylinderMap = std::map<OMKey,I3ExtraGeometryItemCylinder>;
+
+typedef std::map<OMKey, I3ExtraGeometryItemCylinder > I3CylinderMap;
+
+I3_POINTER_TYPEDEFS(I3CylinderMap);
+
+#endif
