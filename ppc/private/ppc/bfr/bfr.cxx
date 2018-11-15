e364f412aad3e7293961d97b668330ceb3c56bdb
Dmitry Chirkin <dima@icecube.wisc.edu>
2018-11-15 21:23:06 +0000

    added experimental code for birefringence calculation and photon propagation
    git-svn-id: http://code.icecube.wisc.edu/svn/highlander/trunk@170580 16731396-06f5-0310-8873-f7f720988828

---
+++ ppc/private/ppc/bfr/bfr.cxx
@@ -0,0 +1,455 @@
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+
+#include <iostream>
+
+#include <gsl/gsl_multifit.h>
+
+using namespace std;
+
+bool verbose=false;
+
+float xrnd(){
+  return drand48();
+}
+
+class myvect{
+public:
+  double x, y, z;
+
+  myvect(){}
+
+  myvect(double x, double y, double z){
+    this->x=x, this->y=y, this->z=z;
+  }
+
+  void set(myvect q){
+    x=q.x, y=q.y, z=q.z;
+  }
+
+  void divide(double p){
+    x/=p, y/=p, z/=p;
+  }
+
+  void multiply(double p){
+    x*=p, y*=p, z*=p;
+  }
+
+  myvect operator* (double p){
+    return myvect(x*p, y*p, z*p);
+  }
+
+  myvect operator/ (double p){
+    return myvect(x/p, y/p, z/p);
+  }
+
+  myvect operator+ (myvect q){
+    return myvect(x+q.x, y+q.y, z+q.z);
+  }
+
+  myvect operator- (myvect q){
+    return myvect(x-q.x, y-q.y, z-q.z);
+  }
+
+  myvect smallest(){
+    myvect temp(fabs(x), fabs(y), fabs(z));
+    switch( temp.z<temp.y? temp.z<temp.x?2:0 : temp.y<temp.x?1:0 ){
+    case 0: temp=myvect(1, 0, 0); break;
+    case 1: temp=myvect(0, 1, 0); break;
+    case 2: temp=myvect(0, 0, 1); break;
+    }
+    return temp;
+  }
+
+  myvect cross(myvect q){
+    return myvect(y*q.z-q.y*z, z*q.x-q.z*x, x*q.y-q.x*y);
+  }
+
+  double dot(myvect q){
+    return x*q.x+y*q.y+z*q.z;
+  }
+
+  double norm(){
+    return sqrt(this->dot(*this));
+  }
+
+  void normalize(){
+    divide(norm());
+  }
+
+  myvect np(myvect q){ // normalized vector perpendicular to this and q (assume |q|=1)
+    // q.normalize();
+    myvect temp=this->cross(q);
+    double size=temp.norm();
+    if(size>0) temp.divide(size);
+    else{
+      temp=this->cross(q.smallest());
+      temp.normalize();
+      myvect aux=temp.cross(q);
+      double phi=0; // 2*M_PI*xrnd();
+      double a=sin(phi), b=cos(phi);
+      temp.x=a*temp.x+b*aux.x;
+      temp.y=a*temp.y+b*aux.y;
+      temp.z=a*temp.z+b*aux.z;
+    }
+    return temp;
+  }
+};
+
+ostream& operator<<(ostream& o, const myvect& q){
+  o << "(" << q.x << "," << q.y << "," << q.z << ")";
+  return o;
+}
+
+class surface:
+  public myvect // normal vector pointing first into second medium
+{
+public:
+  surface() : myvect(){}
+  surface(double x, double y, double z) : myvect(x, y, z){}
+
+  myvect p1, p2;
+
+  void setp(myvect q){
+    q.normalize();
+    p1=this->np(q);
+    p2=this->cross(p1);
+  }
+
+  void setp(){
+    myvect r(fabs(x), fabs(y), fabs(z));
+
+    if(r.y>r.z){
+      if(r.y>r.x){
+	p1 = myvect(y, -x, 0);
+	p2 = myvect(0, -z, y);
+      }
+      else{
+	p1 = myvect(-y, x, 0);
+	p2 = myvect(-z, 0, x);
+      }
+    }
+    else{
+      if(r.z>r.x){
+	p1 = myvect(z, 0, -x);
+	p2 = myvect(0, z, -y);
+      }
+      else{
+	p1 = myvect(-y, x, 0);
+	p2 = myvect(-z, 0, x);
+      }
+    }
+
+    p1.normalize();
+    p2.normalize();
+
+    r = p1-p2; p2 = p1 + p2; p1 = r;
+
+    p1.normalize();
+    p2.normalize();
+  }
+
+  void rand(){
+    double th=M_PI*xrnd(), ph=2*M_PI*xrnd();
+    double ct=cos(th), st=sin(th), cp=cos(ph), sp=sin(ph);
+    x=st*cp, y=st*sp, z=ct;
+  }
+
+  myvect randz(){
+    double ph=2*M_PI*xrnd();
+    double cp=cos(ph), sp=sin(ph);
+    myvect r=p1*cp+p2*sp;
+    return r;
+  }
+};
+
+class photon:
+  public myvect // coordinates
+{
+public:
+  photon() : myvect(){}
+  photon(myvect q) : myvect(q){}
+  photon(double x, double y, double z) : myvect(x, y, z){}
+
+  double n;
+  myvect s; // direction
+  myvect p; // polarization
+  myvect k; // wave vector
+  myvect H; // magnetic field
+  myvect E; // electric field
+  myvect D; // displacement field
+
+  void advance(double r){
+    set(* this + s * (r/s.norm()));
+  }
+};
+
+ostream& operator<<(ostream& o, const photon& q){
+  o << "n = " << q.n << endl;
+  o << "s = " << q.s << endl;
+  o << "p = " << q.p << endl;
+  o << "k = " << q.k << endl;
+  o << "H = " << q.H << endl;
+  o << "E = " << q.E << endl;
+  o << "D = " << q.D << endl;
+  return o;
+}
+
+class medium:
+  public myvect // optical axis
+{
+public:
+  double no,  ne; // refractive indices
+  double beta;
+
+  medium() : myvect(){
+    set_n();
+  }
+
+  medium(myvect q) : myvect(q){
+    set_n();
+  }
+
+  medium(double x, double y, double z) : myvect(x, y, z){
+    set_n();
+  }
+
+  void set_n(){
+    no=1.309, ne=1.313;
+    double aux=ne/no;
+    beta=aux*aux-1;
+  }
+
+  void set_n(double n){
+    no=n, ne=n, beta=0;
+  }
+
+  void set_k(myvect k, photon & o, photon & e){ // o: ordinary; e: extraordinary
+
+    k.normalize();
+
+    o.p=this->np(k);
+    o.n=no;
+    o.k=k;
+    o.k.multiply(o.n);
+
+    e.p=k.cross(o.p);
+    double cs=this->dot(k)/no, sn=this->dot(e.p)/ne;
+    e.n=1/sqrt(cs*cs+sn*sn);
+    e.k=k;
+    e.k.multiply(e.n);
+    if(verbose){
+      cout<<"       "<<o.k<<endl;
+      cout<<"       "<<e.k<<endl;
+    }
+
+    o.D=o.p;
+    o.E=o.p/(no*no);
+    o.H=o.k.cross(o.E);
+    o.s=o.E.cross(o.H);
+
+    e.D=e.p;
+    myvect tmp=this->cross(o.p);
+    e.E=e.D*(this->dot(e.D)/(ne*ne))+tmp*(this->dot(tmp)/(no*no));
+    e.H=e.k.cross(e.E);
+    e.s=e.E.cross(e.H);
+
+  }
+
+  vector<photon> set_k(myvect k, surface s, bool same){
+    vector<photon> result;
+
+    double ky=s.dot(k);
+    myvect Y=s;
+    if(ky>0){
+      ky*=-1;
+      Y.multiply(-1);
+    }
+
+    myvect X=k-Y*ky;
+    double kx=X.norm();
+    if(kx>0) X.divide(kx);
+
+    {
+      double D=no*no-kx*kx;
+      if(D>=0){
+	D=sqrt(D);
+	double ry=same?D:-D;
+	myvect K=X*kx+Y*ry;
+	photon O, E;
+	set_k(K, O, E);
+	result.push_back(O);
+      }
+    }
+
+    {
+      double ax=this->dot(X);
+      double ay=this->dot(Y);
+      double D=ne*ne*(1+beta*ay*ay)-kx*kx*(1+beta*(ax*ax+ay*ay));
+      if(D>=0){
+	D=sqrt(D);
+	double b=-beta*ax*ay*kx, a=1+beta*ay*ay;
+	double r1=(b+D)/a, r2=(b-D)/a;
+	double ry=same?r1:r2;
+	myvect K=X*kx+Y*ry;
+	photon O, E;
+	set_k(K, O, E);
+	result.push_back(E);
+      }
+    }
+
+    return result;
+  }
+};
+
+bool interact(medium one, medium two, surface plane, photon & p){
+  vector<photon> reflected=one.set_k(p.k, plane, true);
+  vector<photon> refracted=two.set_k(p.k, plane, false);
+
+  int refl=reflected.size();
+  int refr=refracted.size();
+  int dim=refl+refr, num=6;
+
+  if(dim<=0){
+    cerr<<"no dice:"<<endl<<"1 = "<<one<<endl<<"2 = "<<two<<endl<<"i = "<<plane<<endl<<p<<endl;
+    return false;
+  }
+
+  myvect r(p);
+  // plane.setp(); //  nominal  basis
+  plane.setp(p.k); // optimized basis
+  double q[num][dim], f[num], x[dim], s[dim];
+
+  for(int i=0; i<=dim; i++){
+    photon& tmp = i<refl ? reflected[i] : i<dim ? refracted[i-refl] : p;
+
+    if(verbose) cout << i << ": " << tmp << endl;
+
+    f[0]=tmp.D.dot(plane);
+    f[1]=tmp.E.dot(plane.p1);
+    f[2]=tmp.E.dot(plane.p2);
+    f[3]=tmp.H.x, f[4]=tmp.H.y, f[5]=tmp.H.z;
+
+    if(i<dim) for(int j=0; j<num; j++) q[j][i]=(i<refl?-1:1)*f[j];
+    s[i]=tmp.s.norm();
+  }
+
+  {
+    gsl_matrix * A = gsl_matrix_alloc(num, dim);
+    gsl_vector * B = gsl_vector_alloc(num);
+
+    for(int i=0; i<num; i++){
+      for(int j=0; j<dim; j++) gsl_matrix_set(A, i, j, q[i][j]);
+      gsl_vector_set(B, i, f[i]);
+    }
+
+    gsl_multifit_linear_workspace * W = gsl_multifit_linear_alloc(num, dim);
+    gsl_vector * X = gsl_vector_alloc(dim);
+    gsl_matrix * C = gsl_matrix_alloc(dim, dim);
+    double c;
+
+    gsl_multifit_linear(A, B, X, C, &c, W);
+    if(verbose) cout<<refl<<" "<<refr<<"  "<<c;
+
+    for(int i=0; i<dim; i++) x[i]=gsl_vector_get(X, i);
+
+    gsl_vector_free(X);
+    gsl_matrix_free(C);
+    gsl_multifit_linear_free(W);
+
+    gsl_vector_free(B);
+    gsl_matrix_free(A);
+  }
+
+  double sum=0;
+  for(int i=0; i<dim; i++){ s[i]=fabs(x[i]*x[i]*s[i]/s[dim]); sum+=s[i]; }
+  if(verbose) cout<<" "<<sum<<endl;
+
+  if(verbose){
+    cout<<"x:"; for(int i=0; i<dim; i++) cout<<" "<<x[i]; cout<<endl;
+    cout<<"f:"; for(int i=0; i<dim; i++) cout<<" "<<s[i]; cout<<endl;
+  }
+
+  bool same;
+  double y=xrnd();
+  sum=0;
+
+  for(int i=0; i<dim; i++){
+    sum+=s[i];
+    if(y<sum || i==dim-1){
+      if(i<refl){
+	same=true;
+	p=reflected[i];
+      }
+      else{
+	same=false;
+	p=refracted[i-refl];
+      }
+      break;
+    }
+  }
+
+  p.set(r);
+  return same;
+}
+
+void test(){
+  verbose=true;
+
+  myvect k(0, 0, 1);
+  medium one(1, 0, 0), two(0, 1, 0); // two media definitions
+  surface plane(0, 0, 1); // interface between two media
+
+  // one.set_n(1), two.set_n(2);
+
+  photon o, e;
+  one.set_k(k, o, e);
+  if(verbose){
+    cout << "o:" << endl << o << endl;
+    cout << "e:" << endl << e << endl;
+  }
+
+  for(int n=0; n<2; n++){
+    photon p=n==0?o:e; // incident photon
+    interact(one, two, plane, p);
+  }
+}
+
+void test2(double p){
+  for(int j=0; j<100000; j++){
+    myvect k(0, 0, 1);
+    medium one, two; // two media definitions
+    surface plane; // interface between two media
+
+    surface flow(sin(p), 0, cos(p)); // direction of ice flow
+    flow.setp();
+
+    one = flow.randz();
+    myvect r(0, 0, 0);
+    photon o(r), e(r);
+    one.set_k(k, o, e);
+    photon p=xrnd()<0.5?o:e;
+
+    two = flow.randz();
+
+    for(int i=0; i<1000; i++){
+      p.advance(1.e-3);
+      plane.rand();
+      bool same=interact(one, two, plane, p);
+      if(same) two = flow.randz();
+      else{
+	one=two; one = flow.randz();
+      }
+    }
+
+    myvect s=p.s;
+    s.normalize();
+    cout<<s.x<<" "<<s.y<<" "<<s.z<<" "<<p.x<<" "<<p.y<<" "<<p.z<<endl;
+  }
+}
+
+main(int arg_c, char *arg_a[]){
+  if(arg_c>1) test2(atof(arg_a[1])*M_PI/180);
+  else test();
+}
