b1b19f99cefbc4ea6e502d24f1f86fb43ab439e3
Tom Stuttard <ts4051@icecube.wisc.edu>
2019-01-22 15:12:40 +0000

    Reverted to using load_pybindings due to issue reading I3GENIEResultDict
    git-svn-id: http://code.icecube.wisc.edu/svn/highlander/trunk@170900 16731396-06f5-0310-8873-f7f720988828

--- genie-icetray/python/__init__.py
+++ genie-icetray/python/__init__.py
@@ -1,7 +1,7 @@
 from icecube.load_pybindings import load_pybindings
 from icecube import icetray, dataclasses # be nice and pull in our dependencies
-# load_pybindings(__name__,__path__)
-icetray.load("libgenie-icetray")
+load_pybindings(__name__,__path__)
+# icetray.load("libgenie-icetray")
 
 
 
