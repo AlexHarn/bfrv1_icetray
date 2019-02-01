3edac54cf74bd3bd638bf86e0cf7f5cc9df75907
Jan Soedingrekso <jan.soedingrekso@icecube.wisc.edu>
2019-02-01 05:43:48 +0000

    do the repeatability test in PROPOSAL with SimplePropagator
    git-svn-id: http://code.icecube.wisc.edu/svn/highlander/trunk@171115 16731396-06f5-0310-8873-f7f720988828

--- PROPOSAL/private/PROPOSAL-icetray/test/Repeatability.cxx
+++ PROPOSAL/private/PROPOSAL-icetray/test/Repeatability.cxx
@@ -22,87 +22,87 @@ static I3Particle make_particle()
     return p;
 }
 
-// TEST(SimplePropagator)
-// {
-//     I3RandomServicePtr rng(new I3SPRNGRandomService(1, 10000, 2));
-
-//     boost::shared_ptr<PROPOSAL::SimplePropagator> prop(
-//         new PROPOSAL::SimplePropagator(I3Particle::MuMinus, "ice", 5e2, -1));
-//     prop->SetRandomNumberGenerator(rng);
-//     double distance = 1e2;
-
-//     std::vector<std::vector<I3Particle> > daughters;
-//     std::vector<I3Particle> primaries;
-//     std::vector<I3FrameObjectPtr> states;
-//     for (int i = 0; i < 100; i++)
-//     {
-//         I3Particle p = make_particle();
-//         p.SetEnergy(std::pow(10, rng->Uniform(3, 6)));
-//         primaries.push_back(p);
-//         states.push_back(rng->GetState());
-//         boost::shared_ptr<std::vector<I3Particle> > d(new std::vector<I3Particle>);
-//         prop->propagate(p, distance, d);
-//         daughters.push_back(*d);
-//     }
-
-//     // Now, replay the simulation and ensure we get the same thing
-
-//     for (size_t i = 0; i < daughters.size(); i++)
-//     {
-//         // Throw out a random subset of events. The remainder should
-//         // be reproducible given the same sequence of random numbers,
-//         // unless of course the propagator keeps secret state.
-//         if (rng->Uniform() < 0.5)
-//             continue;
-//         rng->RestoreState(states[i]);
-//         boost::shared_ptr<std::vector<I3Particle> > d(new std::vector<I3Particle>);
-//         prop->propagate(primaries[i], distance, d);
-//         ENSURE_EQUAL(daughters[i].size(), d->size(), "Same number of particles must be produced");
-//         for (size_t j = 0; j < daughters[i].size(); j++)
-//         {
-//             I3Particle& p1 = daughters[i][j];
-//             I3Particle& p2 = (*d)[j];
-//             ENSURE_EQUAL(p1.GetType(), p2.GetType(), "Secondaries must have the same type");
-//             ENSURE_EQUAL(p1.GetTime(), p2.GetTime(), "Times must be completely identical");
-//             ENSURE_EQUAL(p1.GetEnergy(), p2.GetEnergy(), "Energies must completely identical");
-//         }
-//     }
-// }
-
-TEST(PropagatorService)
+TEST(SimplePropagator)
 {
-    I3RandomServicePtr rng(new I3SPRNGRandomService(1, 10000, 1));
-    I3FrameObjectPtr state = rng->GetState();
+    I3RandomServicePtr rng(new I3SPRNGRandomService(1, 10000, 2));
 
-    PROPOSAL::I3PropagatorServicePROPOSALPtr prop(new PROPOSAL::I3PropagatorServicePROPOSAL);
+    boost::shared_ptr<PROPOSAL::SimplePropagator> prop(
+        new PROPOSAL::SimplePropagator(I3Particle::MuMinus, "ice", 5e2, -1));
     prop->SetRandomNumberGenerator(rng);
-
-    I3PropagatorService::DiagnosticMapPtr frame(new I3PropagatorService::DiagnosticMap);
-    // the dummy I3Frame makes compiler happy, but won't be used
-    I3FramePtr dummy(new I3Frame()); 
+    double distance = 1e2;
 
     std::vector<std::vector<I3Particle> > daughters;
-    for (int i = 0; i < 2; i++)
+    std::vector<I3Particle> primaries;
+    std::vector<I3FrameObjectPtr> states;
+    for (int i = 0; i < 100; i++)
     {
-        I3Particle p              = make_particle();
-        std::vector<I3Particle> d = prop->Propagate(p, frame, dummy);
-        daughters.push_back(d);
+        I3Particle p = make_particle();
+        p.SetEnergy(std::pow(10, rng->Uniform(3, 6)));
+        primaries.push_back(p);
+        states.push_back(rng->GetState());
+        boost::shared_ptr<std::vector<I3Particle> > d(new std::vector<I3Particle>);
+        prop->propagate(p, distance, d);
+        daughters.push_back(*d);
     }
 
-    rng->RestoreState(state);
+    // Now, replay the simulation and ensure we get the same thing
 
     for (size_t i = 0; i < daughters.size(); i++)
     {
-        I3Particle p              = make_particle();
-        std::vector<I3Particle> d = prop->Propagate(p, frame, dummy);
-        ENSURE_EQUAL(daughters[i].size(), d.size());
+        // Throw out a random subset of events. The remainder should
+        // be reproducible given the same sequence of random numbers,
+        // unless of course the propagator keeps secret state.
+        if (rng->Uniform() < 0.5)
+            continue;
+        rng->RestoreState(states[i]);
+        boost::shared_ptr<std::vector<I3Particle> > d(new std::vector<I3Particle>);
+        prop->propagate(primaries[i], distance, d);
+        ENSURE_EQUAL(daughters[i].size(), d->size(), "Same number of particles must be produced");
         for (size_t j = 0; j < daughters[i].size(); j++)
         {
             I3Particle& p1 = daughters[i][j];
-            I3Particle& p2 = d[j];
-            ENSURE_EQUAL(p1.GetType(), p2.GetType());
-            ENSURE_EQUAL(p1.GetTime(), p2.GetTime());
-            ENSURE_EQUAL(p1.GetEnergy(), p2.GetEnergy());
+            I3Particle& p2 = (*d)[j];
+            ENSURE_EQUAL(p1.GetType(), p2.GetType(), "Secondaries must have the same type");
+            ENSURE_EQUAL(p1.GetTime(), p2.GetTime(), "Times must be completely identical");
+            ENSURE_EQUAL(p1.GetEnergy(), p2.GetEnergy(), "Energies must completely identical");
         }
     }
 }
+
+// TEST(PropagatorService)
+// {
+//     I3RandomServicePtr rng(new I3SPRNGRandomService(1, 10000, 1));
+//     I3FrameObjectPtr state = rng->GetState();
+
+//     PROPOSAL::I3PropagatorServicePROPOSALPtr prop(new PROPOSAL::I3PropagatorServicePROPOSAL);
+//     prop->SetRandomNumberGenerator(rng);
+
+//     I3PropagatorService::DiagnosticMapPtr frame(new I3PropagatorService::DiagnosticMap);
+//     // the dummy I3Frame makes compiler happy, but won't be used
+//     I3FramePtr dummy(new I3Frame()); 
+
+//     std::vector<std::vector<I3Particle> > daughters;
+//     for (int i = 0; i < 2; i++)
+//     {
+//         I3Particle p              = make_particle();
+//         std::vector<I3Particle> d = prop->Propagate(p, frame, dummy);
+//         daughters.push_back(d);
+//     }
+
+//     rng->RestoreState(state);
+
+//     for (size_t i = 0; i < daughters.size(); i++)
+//     {
+//         I3Particle p              = make_particle();
+//         std::vector<I3Particle> d = prop->Propagate(p, frame, dummy);
+//         ENSURE_EQUAL(daughters[i].size(), d.size());
+//         for (size_t j = 0; j < daughters[i].size(); j++)
+//         {
+//             I3Particle& p1 = daughters[i][j];
+//             I3Particle& p2 = d[j];
+//             ENSURE_EQUAL(p1.GetType(), p2.GetType());
+//             ENSURE_EQUAL(p1.GetTime(), p2.GetTime());
+//             ENSURE_EQUAL(p1.GetEnergy(), p2.GetEnergy());
+//         }
+//     }
+// }
