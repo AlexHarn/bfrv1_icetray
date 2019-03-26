2e0bb060237bf478fe2ba60177eff160c89ceeb0
Alex Olivas <olivas@icecube.umd.edu>
2019-03-26 17:06:35 +0000

    made a little more progress on the low energy update feature request for a GCD sanity checker.  gonna take some more work, so pushing to the next milestone.
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@172316 16731396-06f5-0310-8873-f7f720988828

--- sim-services/python/gcd_validation/gcd_sanity_checker.py
+++ sim-services/python/gcd_validation/gcd_sanity_checker.py
@@ -6,11 +6,11 @@ class I3GCDSanityChecker(icetray.I3Module):
     def __init__(self, context):
         super().__init__(context)
 
-        default_url = 'http://simprod.icecube.wisc.edu/downloads/GCD/'
-        self.AddParameter('GCDFilename', 'Name of GCD file to use.', None)
-        self.AddParameter('LocalPath', 'Location of GCD file.', None)
-        self.AddParameter('URL', 'URL to pull GCD files from', default_url)
+        self.__url = 
+        self.AddParameter('GCDFilename', 'Fully resloved filename of GCD file.', None)
+        self.AddParameter('Prescale', 'Only check every Nth Q-frame', None)
 
+        self.__q_frame_counter = 0
         self.__validated = False
         self.__stops_called = {'Geometry': 0,
                                'Calbration': 0,
@@ -18,17 +18,12 @@ class I3GCDSanityChecker(icetray.I3Module):
         
     def Configure(self):
         self.gcd_filename = self.GetParameter('GCDFilename')
-        self.local_path = self.GetParameter('LocalPath')
-        self.url = self.GetParameter('URL')        
+        self.prescale = self.GetParameter('Prescale')
         
-        fn = os.path.join(self.local_path, self.filename)
-        if not os.path.exists(fn):
-            url = self.url + self.gcd_filename
-            response = urllib.urlopen(url)
-            with open(fn, 'wb') as f:
-                f.write(content.read())
+        if not os.path.exists(self.filename):
+            icetray.logging.log_fatal("GCD file %s not found." % self.filename)
                 
-        with dataio.I3File(fn) as f:
+        with dataio.I3File(self.filename) as f:
             self.geometry_frame = None
             self.calibration_frame = None
             self.detector_status_frame = None
@@ -44,12 +39,11 @@ class I3GCDSanityChecker(icetray.I3Module):
 
         # We'll need to get the checksums from some location
         try:
-            base_fn = self.gcd_filename.split('.')[0]
-            url = self.url + base_fn + '.checksums'
+            url = self.__url + 'checksums'
             response = urllib.urlopen(url)
             self.checksums = pickle.loads(response.read())
         except:
-            print("Something went wrong checking/loading checksums")
+            icetray.logging.log_fatal("Something went wrong checking/loading checksums")
             
     def Geometry(self, frame):
         frame['I3Geometry'] = self.geometry_frame
@@ -66,26 +60,41 @@ class I3GCDSanityChecker(icetray.I3Module):
         self.__stops_called['DetectorStatus'] += 1
         self.PushFrame(frame)
 
+    def __check_frame(self, frame):
+        for frame_key in ['I3Geometry', 'I3Calibration', 'I3DetectorStatus']:
+            # TODO: Need to expose something via pybindings; either the underlying
+            #       state via pickle (or buffer) or expose a hash (bound to __hash__, perhaps).
+            #       There's really no reliable way, that I can see now, to get the
+            #       checksum of the memory representation of frame objects.
+            m = hashlib.sha256()
+            obj = frame[frame_key]
+            m.update(obj.buffer) # <--- need pybindings for this.
+            if m.hexdigest() != self.checksums[frame_key]:
+                icetray.logging.log_fatal('The %s frame has been altered.' % frame_key)
+
+            ### another way might be, if __hash__ is bound
+            # checksum = hash(frame[frame_key]) # <-- says nothing about the algo though
+            ### ...or...
+            # checksum = frame[frame_key].sha256()  # <-- need bindings too
+            # if checksum != self.checksums[frame_key]:
+            #    icetray.logging.log_fatal('The %s frame has been altered.' % frame_key)
+        
     def DAQ(self, frame):
         '''
         Only check the G,C, and D frames at the beginning of the first Q frame.
         '''
-        if self.__validated:
+        self.__q_frame_counter += 1
+        
+        if self.__validated or self.__q_frame_counter % self.prescale:
             self.PushFrame(frame)
             return
 
-        # At this point we should have access to all frames
-        for frame_key in ['I3Geometray', 'I3Calibration', 'I3DetectorStatus']:
-            m = hashlib.sha256()
-            m.update(frame[frame_key])
-            if m.hexdigest() != self.checksums[frame_key]:
-                print('The %s frame is different' % frame_key)
-            else:
-                print('%s checks out.' % frame_key)
-
+        self.__check_frame(frame)
         self.PushFrame(frame)
                 
     def Finish(self, frame):
+        self.__check_frame(frame)
+
         for stop, ncalls in self.__stops_called.items():
             if ncalls > 1 :
-                print("WARNING: %s called %d times." % (stop, ncalls))
+                icetray.logging.log_fatal("%s called %d times." % (stop, ncalls))
