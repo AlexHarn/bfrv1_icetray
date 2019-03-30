93019f471a34665a234e105130a3b1a9b75c7720
Alex Olivas <olivas@icecube.umd.edu>
2019-03-30 00:54:47 +0000

    error reports often go unnoticed.
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@172382 16731396-06f5-0310-8873-f7f720988828

--- production-histograms/python/db.py
+++ production-histograms/python/db.py
@@ -1,21 +1,19 @@
 from os import environ
 from os.path import join
 from os.path import exists
+from icecube import icetray
     
 def create_simprod_db_client(database_url = 'mongodb-simprod.icecube.wisc.edu',
                              dbuser = 'DBadmin',
                              password_path = join(environ["HOME"], '.mongo')):
     
-    from icecube import icetray
     try:
         from pymongo import MongoClient
     except ImportError:
-        icetray.logging.log_error("PyMongo not installed.")
-        return
+        icetray.logging.log_fatal("PyMongo not installed.")
 
     if not exists(password_path):
-        icetray.logging.log_error("Credentials not found.")
-        return
+        icetray.logging.log_fatal("Credentials not found.")
         
     f = open(password_path)
     uri = "mongodb://%s:%s@%s" % (dbuser, f.readline().strip(), database_url)
