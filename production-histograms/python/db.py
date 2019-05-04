6ae8d860204f277790a75a2232cd9246de47f7e1
Alex Olivas <olivas@icecube.umd.edu>
2019-05-04 14:49:23 +0000

    added a helper class for histogram db access.
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@173031 16731396-06f5-0310-8873-f7f720988828

--- production-histograms/python/db.py
+++ production-histograms/python/db.py
@@ -24,4 +24,35 @@ def create_simprod_db_client(database_url = 'mongodb-simprod.icecube.wisc.edu',
 
     return client
 
+class SimProdHistogramDB:
+    def __init__(self, collection_name = None):
 
+        try:
+            from pymongo import MongoClient
+        except ImportError:
+            logging.critical("PyMongo not installed.")
+
+        database_url = 'mongodb-simprod.icecube.wisc.edu',
+        uri = "mongodb://icecube:skua@%s" % (database_url)
+        client = MongoClient(uri)
+        self.db = client['simprod_histograms']
+
+        if collection_name:
+            self.set_collection(collection_name)
+
+    def collection_names(self):
+        return self.db.list_collection_names()
+        
+    def set_collection(self, collection_name):
+        self.collection_name = collection_name
+        self.collection = self.db[self.collection_name]
+            
+    def histograms(self):
+        return [h['name'] for h in self.collection.find({'name' : {'$ne':'filelist'}})] 
+
+    def get_histogram(self, collection_name, histogram_name):
+        return self.collection.find_one({'name' : histogram_name})
+
+    def get_filelist(self, collection_name):
+        return self.collection.find_one({'name':'filelist'})['files'] 
+        
