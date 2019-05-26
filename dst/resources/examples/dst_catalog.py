3814df94568c67760042841d4f84fd408c0bd02e
Juan Carlos Díaz Vélez <juancarlos@icecube.wisc.edu>
2019-05-26 22:14:03 +0000

    DST processing scripts
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@173479 16731396-06f5-0310-8873-f7f720988828

---
+++ dst/resources/examples/dst_catalog.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+#
+#
+import os,sys
+import os.path 
+from os.path import expandvars
+
+from optparse import OptionParser
+
+
+import glob
+import json
+import urllib
+import urllib2
+import os,sys
+
+
+def format_dir(s):
+    date = s.split()[0]
+    y, m, d = map(int,date.split("-"))
+    return "%02u%02u" % (m,d)
+
+def format_date(s):
+    date = s.split()[0]
+    y, m, d = map(int,date.split("-"))
+    return "%04u-%02u-%02u" % (y,m,d)
+    
+def getgrl(start,end):
+    url = 'https://live.icecube.wisc.edu/snapshot-export/'
+    
+    params = {'user': 'icecube', 
+          'pass': 'skua', 
+          'start_date': start,
+          'end_date': end}
+    if type(start) == int:
+    	params = {'user': 'icecube', 
+          'pass': 'skua', 
+          'start_run': start,
+          'end_run': end}
+
+
+    # Fetch the JSON data (hits the database)
+    req = urllib2.Request(url, urllib.urlencode(params))
+    response = urllib2.urlopen(req).read()
+
+    # Parse JSON into a python dict and print some of it
+    d = json.loads(response)
+    goodruns = []
+    for r in d['runs']:
+    	if r['good_i3']: goodruns.append(r)
+    return goodruns
+
+
+def getdirs(run):
+    # Define the view you want to hit (in this case 'run_info')
+    url = 'https://live.icecube.wisc.edu/run_info/'
+
+    # Define parameters to pass to the view. Keywords are different for each view,
+    # except for 'user' and 'pass' which are mandatory for all views.
+    params = {'user': 'icecube', 
+          'pass': 'skua', 
+          'run_number': run}
+
+
+
+    # Fetch the JSON data (hits the database)
+    req = urllib2.Request(url, urllib.urlencode(params))
+    response = urllib2.urlopen(req).read()
+
+    # Parse JSON into a python dict and print some of it
+    d = json.loads(response)
+    start = format_dir(d['start'])
+    stop = format_dir(d['stop'])
+    r = []
+    r.append("%s"%start)
+    if start != stop:
+        r.append("%s"%stop)
+    return r,format_date(d['start'])
+
+
+if __name__ == "__main__":
+	usage = "usage: %prog [options] inputfile"
+	parser = OptionParser(usage)
+
+	parser.add_option("-p", "--procnum",default=0,type=int,
+			  dest="PROCNUM", help="Job number")
+	parser.add_option("-o", "--outdir",default="./",
+			  dest="OUTDIR", help="Write output to OUTDIR")
+	parser.add_option("-f", "--format",default=".hdf5",
+			  dest="FORMAT", help="Write output format (.root, .hdf5)")
+	parser.add_option("-g", "--gcd",default="auto",
+			  dest="GCDFILE", help="Read geometry from GCDFILE (.i3{.gz} format).")
+	parser.add_option("-d", "--dir",default="/data/exp/IceCube/YEAR/filtered/PFFilt",
+			  dest="DIR", help="Directory")
+	parser.add_option("-c", "--chunk-size",default=1,type=int,
+			  dest="CHUNKSIZE", help="Chunk of data to process")
+	parser.add_option("-v", "--dst-version",default="dst16",
+			  dest="DSTVERSION", help="DST version to read (dst13,dst16)")
+	parser.add_option("-s", "--start-run",type=int, default=0,
+			  dest="START", help="Start run")
+	parser.add_option("-e", "--end-run",type=int, default=0,
+			  dest="END", help="End run")
+	parser.add_option("-u", "--url", default="gsiftp://gridftp.icecube.wisc.edu",
+			  dest="URL", help="GridFTP URL")
+	parser.add_option("-j", "--json",default="dst_catalog.json",
+			  dest="JSON", help="JSON catalog file")
+
+	(options,args) = parser.parse_args()
+
+	# Take the input file from the command line arguments
+	runs = getgrl(options.START ,options.END)
+
+	tasks = []
+	current_task = -1
+	current_file = 0
+	current_subrun = 0
+	gcdfile = None
+	dstfile = options.OUTDIR+"/ic86_simpleDST_%(date)s_part%(subrun)02u%(format)s" 
+
+	if options.DIR:
+	    for run in runs:
+	        print ("This is run number %u." %run['run']) 
+	        current_subrun = 0
+	        dirs,date = getdirs(run['run'])
+	        globlist = []
+	        year,month,day = date.split('-')
+	        for d in dirs:
+	    	    prefix = options.DIR.replace('YEAR',year)+"/"+d
+	    	    prefix += "/PFFilt_PhysicsFiltering_Run%08d_Subrun00000000_" % run['run']
+	    	    globlist.extend(glob.glob(prefix+"*.tar.bz2"))
+	        globlist.sort()
+
+	        gcdprefix = options.DIR.replace('YEAR',year)+"/*"
+	    	gcdprefix = gcdprefix.replace("PFFilt","level2/OfflinePreChecks/DataFiles/")
+	    	gcdprefix += "/Level2_IC86.*_data_Run%08u_*.i3.zst" % run['run']
+	    	gcdfile = glob.glob(gcdprefix)[0]
+	    	dstdict = {"year":year,"month":month,"day":day,"date":date,"subrun":current_subrun,"format":options.FORMAT}
+            tasks.append({"files":[],"gcd":options.URL+gcdfile,"dstfile":dstfile%dstdict,"dstversion":options.DSTVERSION})
+            current_task += 1
+
+            for f in globlist:
+                tasks[current_task]['files'].append(options.URL+f)
+                current_file += 1
+
+                if current_file >= options.CHUNKSIZE:
+                    current_subrun += 1
+                    current_task += 1
+                    current_file = 0
+                    dstdict = {"year":year,"month":month,"day":day,"date":date,"subrun":current_subrun,"format":options.FORMAT}
+                    tasks.append({"files":[],"gcd":options.URL+gcdfile,"dstfile":dstfile%dstdict,"dstversion":options.DSTVERSION})
+
+	
+	json.dump({"tasks":tasks},open(options.JSON,'w'))
+
--- dst/resources/examples/dstprocess.py
+++ dst/resources/examples/dstprocess.py
@@ -2,82 +2,18 @@
 #
 #
 import os,sys
+import json
+import urllib
+import urllib2
+from optparse import OptionParser
+
 from I3Tray import *
 import os.path 
 from os.path import expandvars
 import icecube.icetray
-
 from icecube import icetray, dataclasses, dataio, dst
-from optparse import OptionParser
+import gridftp
 
-
-import json
-import urllib
-import urllib2
-import os,sys
-
-
-def format_dir(s):
-    date = s.split()[0]
-    y, m, d = map(int,date.split("-"))
-    return "%02u%02u" % (m,d)
-
-def format_date(s):
-    date = s.split()[0]
-    y, m, d = map(int,date.split("-"))
-    return "%04u-%02u-%02u" % (y,m,d)
-    
-def getgrl(start,end):
-    url = 'https://live.icecube.wisc.edu/snapshot-export/'
-    
-    params = {'user': 'icecube', 
-          'pass': 'skua', 
-          'start_date': start,
-          'end_date': end}
-    if type(start) == int:
-    	params = {'user': 'icecube', 
-          'pass': 'skua', 
-          'start_run': start,
-          'end_run': end}
-
-
-    # Fetch the JSON data (hits the database)
-    req = urllib2.Request(url, urllib.urlencode(params))
-    response = urllib2.urlopen(req).read()
-
-    # Parse JSON into a python dict and print some of it
-    d = json.loads(response)
-    goodruns = []
-    for r in d['runs']:
-    	if r['good_i3']: goodruns.append(r)
-    return goodruns
-
-
-def getdirs(run):
-    # Define the view you want to hit (in this case 'run_info')
-    url = 'https://live.icecube.wisc.edu/run_info/'
-
-    # Define parameters to pass to the view. Keywords are different for each view,
-    # except for 'user' and 'pass' which are mandatory for all views.
-    params = {'user': 'icecube', 
-          'pass': 'skua', 
-          'run_number': run}
-
-
-
-    # Fetch the JSON data (hits the database)
-    req = urllib2.Request(url, urllib.urlencode(params))
-    response = urllib2.urlopen(req).read()
-
-    # Parse JSON into a python dict and print some of it
-    d = json.loads(response)
-    start = format_dir(d['start'])
-    stop = format_dir(d['stop'])
-    r = []
-    r.append("%s"%start)
-    if start != stop:
-        r.append("%s"%stop)
-    return r,format_date(d['start'])
 
 
 if __name__ == "__main__":
@@ -86,95 +22,35 @@ if __name__ == "__main__":
 
 	parser.add_option("-p", "--procnum",default=0,type=int,
 			  dest="PROCNUM", help="Job number")
-	parser.add_option("-o", "--outdir",default="./",
-			  dest="OUTDIR", help="Write output to OUTDIR")
-	parser.add_option("-f", "--format",default=".root",
-			  dest="FORMAT", help="Write output format (.root, .hdf5)")
-	parser.add_option("-i", "--infile", action="append",
-			  dest="INFILELIST", help="Read (.i3{.gz})")
-	parser.add_option("-g", "--gcd",default="auto",
-			  dest="GCDFILE", help="Read geometry from GCDFILE (.i3{.gz} format).")
-	parser.add_option("-r", "--run",default=0,type=int,
-			  dest="RUN", help="Run")
-	parser.add_option("-d", "--dir",default="",
-			  dest="DIR", help="Directory")
-	parser.add_option("-u", "--chunk",default=0,type=int,
-			  dest="CHUNK", help="Chunk of data to process")
-	parser.add_option("-n", "--nchunks",default=1,type=int,
-			  dest="NCHUNK", help="Number of chunks")
-	parser.add_option("-v", "--dst-version",default="dst16",
-			  dest="DSTVERSION", help="DST version to read (dst13,dst16)")
 	parser.add_option("-x", "--extract-to-frame",action="store_true", default=False,
 			  dest="X2FRAME", help="Extract DST to standar I3Reco objects in frame")
 	parser.add_option("-c", "--cut",action="store_true", default=False,
 			  dest="CUT", help="Apply cuts")
-	parser.add_option("-s", "--start-run",type=int, default=0,
-			  dest="START", help="Start run")
-	parser.add_option("-e", "--end-run",type=int, default=0,
-			  dest="END", help="End run")
+	parser.add_option("-j", "--json",default="dst_catalog.json",
+			  dest="JSON", help="JSON catalog file")
 
 	(options,args) = parser.parse_args()
+	
+	jsonfile = json.load(open(options.JSON,'r'))
+	taskinfo = jsonfile['tasks'][options.PROCNUM]
 
-	if options.DSTVERSION not in ("dst13", "dst16"):
-	    raise RuntimeError("unsupported DST version %s. dst13 or dst16" % options.DSTVERSION)
-	if options.DSTVERSION == 'dst13':
-	   from icecube.dst import ExtractDST13
-	else:
-	   from icecube.dst import ExtractDST
+	dstfile = os.path.basename(taskinfo['dstfile']).encode("utf8")
+	dstdir = os.path.dirname(taskinfo['dstfile'])
 
-	# Take the input file from the command line arguments
-	if options.START > 0 and options.RUN == 0 and options.PROCNUM > -1:
-	    if options.END < options.START: 
-	        options.RUN = options.START
-	    else:
-	        runs = getgrl(options.START ,options.END)
-	        NPROC = len(runs)*options.NCHUNK
-	        run_index = options.PROCNUM/options.NCHUNK
-	        options.RUN = runs[run_index]['run']
-	    options.CHUNK = options.PROCNUM%options.NCHUNK
+	ftp = gridftp.GridFTP()
+	filelist = map(lambda x:x.encode("utf8"),[taskinfo['gcd']]+taskinfo['files'])
+	local_path = os.getcwd()
+	local_filelist = map(lambda x:local_path+"/"+os.path.basename(x),filelist)
+	for source,dest in zip(filelist,local_filelist):
+	    print("downloading file %s" % source)
+	    ftp.get(source,filename=dest)
 
-	if options.RUN and options.DIR:
-	    import glob
-	    #globlist = glob.glob(options.DIR+"/PFFilt_PhysicsTrig_PhysicsFiltering_Run%08u*.tar.bz2" % options.RUN)
-	    dirs,date = getdirs(options.RUN)
-	    globlist = []
-	    year = date.split('-')[0]
-	    for d in dirs:
-	    	prefix = options.DIR.replace('YEAR',year)+"/"+d
-	    	prefix += "/PFFilt_PhysicsFiltering_Run%08d_Subrun00000000_" % options.RUN
-	    	globlist.extend(glob.glob(prefix+"*.tar.bz2"))
-	    globlist.sort()
-
-	    if options.GCDFILE == 'auto':
-	    	gcdprefix = options.DIR.replace('YEAR',year)+"/*"
-	    	gcdprefix = gcdprefix.replace("PFFilt","level2/OfflinePreChecks/DataFiles/")
-	    	gcdprefix += "/Level2_IC86.*_data_Run%08u_*.i3.zst" % options.RUN
-	    	options.GCDFILE = glob.glob(gcdprefix)[0]
-            
-	    
-	    chunksize = len(globlist)/options.NCHUNK
-	    print chunksize, len(globlist),options.NCHUNK, options.RUN
-
-	    start = chunksize*options.CHUNK
-	    end = chunksize*(options.CHUNK+1)
-
-	    if end < len(globlist) and end > len(globlist)+chunksize: 
-		end = len(globlist)
-
-	    print "processing files" , start, "to", end
-	    i3filelist = globlist[start:end]
-
-	    FileList = [options.GCDFILE]+i3filelist
-	else:
-	    FileList = [options.GCDFILE]+options.INFILELIST
-	dstfile = options.OUTDIR+"/ic86_simpleDST_%s_part%02u%s" %(date,options.NCHUNK,options.FORMAT)
-	dstfile = "test.hdf5"
 
 	tray = I3Tray()
 
 
 	tray.AddModule("I3Reader","i3reader")(
-	    ("FileNameList", FileList),
+	    ("FileNameList", local_filelist)
 	    )
 
 
@@ -184,13 +60,14 @@ if __name__ == "__main__":
 	    ("WritePFrame", False)
 	    )
 
-
-	if options.DSTVERSION == 'dst13':
+	if taskinfo['dstversion'] == 'dst13':
+	   from icecube.dst import ExtractDST13
 	   tray.AddSegment(ExtractDST13,"extractdst",
 		    dst_output_filename = dstfile,
 		    extract_to_frame = options.X2FRAME,
 		    dstname = "I3DST13")
 	else:
+	   from icecube.dst import ExtractDST
 	   tray.AddSegment(ExtractDST,"extractdst",
 		    dst_output_filename = dstfile,
 		    extract_to_frame = options.X2FRAME,
@@ -208,3 +85,6 @@ if __name__ == "__main__":
 	#
 
 	tray.Execute()
+
+	print("uploading dstfile %(dstfile)s" % taskinfo)
+	ftp.put(taskinfo['dstfile'],filename=local_path+"/"+dstfile)
