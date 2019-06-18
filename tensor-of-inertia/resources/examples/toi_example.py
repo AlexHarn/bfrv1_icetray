032eacb798647bd1c02e4aba94d82b95cde894a5
Kevin Meagher <kmeagher@icecube.wisc.edu>
2019-06-18 20:33:03 +0000

    change test data for new test data see #1353
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@174060 16731396-06f5-0310-8873-f7f720988828

--- tensor-of-inertia/resources/examples/toi_example.py
+++ tensor-of-inertia/resources/examples/toi_example.py
@@ -6,20 +6,26 @@ I3File with the result added to the frame.  This example assumes the input file
 has no G, C, or D frames, so a GCD must be supplied separately.
 '''
 
+import os
+I3_TESTDATA=os.environ['I3_TESTDATA']
+
 from optparse import OptionParser
-
 parser = OptionParser()
-
 parser.add_option("-i","--input_file",
-                  dest="INPUTFILE", 
+                  dest="INPUTFILE",
+                  default=os.path.join(I3_TESTDATA,"sim",
+                                       "Level2_IC86.2011_corsika.010281.001664.00.i3.bz2"),
                   help="Name of input I3File.")
 
 parser.add_option("-g","--gcd_file",
-                  dest="GCDFILE", 
+                  dest="GCDFILE",
+                  default=os.path.join(I3_TESTDATA,"GCD",
+                                       "GeoCalibDetectorStatus_2012.56063_V0.i3.gz"),
                   help="Name of input GCD file.")
 
 parser.add_option("-o","--output_file",
-                  dest="OUTPUTFILE", 
+                  dest="OUTPUTFILE",
+                  default="toi_example.i3",
                   help="Name of output I3File.")
 
 parser.add_option("-n","--nframes",
