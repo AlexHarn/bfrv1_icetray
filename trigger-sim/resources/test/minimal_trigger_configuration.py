d4dbd0bb7384a7e8c2c98e75bba0cc5b1b3e24c5
Alex Olivas <olivas@icecube.umd.edu>
2019-06-20 21:58:16 +0000

    adapt to modern pythonic script style.
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@174118 16731396-06f5-0310-8873-f7f720988828

--- trigger-sim/resources/test/minimal_trigger_configuration.py
+++ trigger-sim/resources/test/minimal_trigger_configuration.py
@@ -8,34 +8,20 @@ from os.path import expandvars
 
 tray = I3Tray()
 
-if os.environ.get("I3_TESTDATA") :
-    i3_testdata = expandvars("$I3_TESTDATA") 
-else :
-    i3_testdata = expandvars("$I3_TESTDATA")
-  
+i3_testdata = expandvars("$I3_TESTDATA") 
 gcd_file = i3_testdata + "/GCD/GeoCalibDetectorStatus_2016.57531_V0.i3.gz"
 
-tray.AddModule("I3InfiniteSource", "source", 
-               prefix = gcd_file)
+tray.AddModule("I3InfiniteSource", prefix = gcd_file)               
 
-tray.AddModule("SimpleMajorityTrigger","IISMT8")(
-    ("TriggerConfigID",1006)
-    )
+tray.AddModule("SimpleMajorityTrigger","IISMT8",               
+               TriggerConfigID = 1006)
 
-tray.AddModule("SimpleMajorityTrigger","DCSMT3")(
-    ("TriggerConfigID",1011)
-    )
+tray.AddModule("SimpleMajorityTrigger","DCSMT3",
+               TriggerConfigID = 1011)
 
 tray.AddModule("ClusterTrigger","string")    
-
-#tray.AddModule("CylinderTrigger","cyl")
-
 tray.AddModule("SlowMonopoleTrigger","slop")
-
-tray.AddModule("SimpleMajorityTrigger","ITSMT6")(    
-    ("TriggerConfigID",102)
-    )
-
-
+tray.AddModule("SimpleMajorityTrigger","ITSMT6",
+               TriggerConfigID = 102)
 
 tray.Execute(10)
