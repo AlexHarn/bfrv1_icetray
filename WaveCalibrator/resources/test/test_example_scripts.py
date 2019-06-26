21ca39c650c0b9ae716ef1a4d2c49f1b288fa480
Kevin Meagher <kmeagher@icecube.wisc.edu>
2019-06-26 19:14:06 +0000

    change path of test data
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@174232 16731396-06f5-0310-8873-f7f720988828

--- WaveCalibrator/resources/test/test_example_scripts.py
+++ WaveCalibrator/resources/test/test_example_scripts.py
@@ -12,7 +12,7 @@ class TestSimpleExample(I3Test.TestExampleScripts):
         '''
         Test that runs the one example script in this project.
         '''
-        gcd_file = self.I3_TESTDATA + "/sim/GeoCalibDetectorStatus_2013.56429_V1.i3.gz"
+        gcd_file = self.I3_TESTDATA + "/GCD/GeoCalibDetectorStatus_2013.56429_V1.i3.gz"
         input_file = self.I3_TESTDATA + "/sim/Level2_IC86.2011_corsika.010281.001664.00.i3.bz2"
         output_file = "./output.i3.gz"
         self.run_example('simple_example.py', gcd_file, input_file, output_file)
