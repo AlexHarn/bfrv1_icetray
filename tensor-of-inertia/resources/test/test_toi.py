998a83a70b9a4aa45cce2d3f6e98d469bb1af75d
Andres Medina <andres.medina@icecube.wisc.edu>
2019-10-18 20:11:16 +0000

    update on test_toi.py because the location of the GCD was incorrect
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@176270 16731396-06f5-0310-8873-f7f720988828

--- tensor-of-inertia/resources/test/test_toi.py
+++ tensor-of-inertia/resources/test/test_toi.py
@@ -37,7 +37,7 @@ class TestStringMethods(unittest.TestCase):
         tray = I3Tray()
 
         I3_TESTDATA=os.environ['I3_TESTDATA']
-        gcd_file = os.path.join(I3_TESTDATA,"GCD","GeoCalibDetectorStatus_2012.56063_V0.i3.gz")
+        gcd_file = os.path.join(I3_TESTDATA,"sim","GeoCalibDetectorStatus_2012.56063_V0.i3.gz")
         input_file = os.path.join(I3_TESTDATA,"sim","Level2_IC86.2011_corsika.010281.001664.00.i3.bz2")
         
         tray.AddModule("I3Reader", FileNameList = [gcd_file,input_file] )
