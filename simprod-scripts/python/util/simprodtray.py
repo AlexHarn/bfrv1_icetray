0818204fd2f9fe535c94a1baf56690583add774c
Eric Evans <eevans@icecube.wisc.edu>
2019-11-21 17:59:51 +0000

    modernized clsim scripts
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@177325 16731396-06f5-0310-8873-f7f720988828

---
+++ simprod-scripts/python/util/simprodtray.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+
+"""
+ Generic SimProd IceTray Execution and Utilities
+"""
+
+import logging
+from icecube import icetray, dataio, phys_services, dataclasses
+from I3Tray import I3Tray
+from icecube.simprod.util import WriteI3Summary, ReadI3Summary
+import os
+
+# Set up default/root logging level
+logging.basicConfig()
+rootLogger = logging.getLogger('')
+rootLogger.setLevel(logging.DEBUG)
+
+
+def _add_i3_summary_service(tray, summaryfile, summaryin):
+    if not summaryin is None:
+        summary = summaryin
+    elif os.path.exists(summaryfile):
+        summary = ReadI3Summary(summaryfile)
+    else:
+        summary = dataclasses.I3MapStringDouble()
+    tray.context['I3SummaryService'] = summary
+
+
+def _add_i3_random_service(tray, usegslrng, seed, nstreams, streamnum):
+    if (nstreams is None) or (streamnum is None):
+        rand = phys_services.I3GSLRandomService(seed=seed)
+    elif usegslrng:
+        rand = phys_services.I3GSLRandomService(seed=seed * nstreams + streamnum)
+    else:
+        rand = phys_services.I3SPRNGRandomService(seed=seed, nstreams=nstreams, streamnum=streamnum)
+    tray.context['I3RandomService'] = rand
+
+
+def _add_i3_writer(tray, outputfile, outputstreams, outputskipkeys):
+    if outputskipkeys:
+        tray.AddModule("I3Writer", "writer", filename=outputfile,
+                       Streams=outputstreams, SkipKeys=outputskipkeys)
+    else:
+        tray.AddModule("I3Writer", "writer", filename=outputfile,
+                       Streams=outputstreams)
+
+
+def _execute(tray, executionmaxcount):
+    if executionmaxcount:
+        tray.Execute(executionmaxcount)
+    else:
+        tray.Execute()
+
+
+def _save_stats(tray, summary, stats):
+    for k in tray.Usage():
+        stats[str(k.key()) + ":usr"] = k.data().usertime
+        summary[str(k.key()) + ":usr"] = k.data().usertime
+
+        stats[str(k.key()) + ":sys"] = k.data().systime
+        summary[str(k.key()) + ":sys"] = k.data().systime
+
+        stats[str(k.key()) + ":ncall"] = k.data().ncall
+        summary[str(k.key()) + ":ncall"] = k.data().ncall
+
+
+def _print_stats(stats, logger, simulationname):
+    header = "Stats produced by {0}:".format(simulationname)
+    logger.info(header)
+    logger.info("-" * len(header))
+    for k, v in stats.items():
+        logger.info("%s,%s" % (k, v))
+
+
+OUTPUT_STREAMS = [icetray.I3Frame.TrayInfo,
+                  icetray.I3Frame.DAQ,
+                  icetray.I3Frame.Stream('S'),
+                  icetray.I3Frame.Stream('M')]
+
+
+def RunI3Tray(params, configure_tray, simulationname, stats=dict(),
+              summaryfile="", summaryin=None, doprintstats=True,
+              inputfilenamelist=None, outputfile="", outputskipkeys=None,
+              outputstreams=OUTPUT_STREAMS, executionmaxcount=None,
+              seed=None, nstreams=None, streamnum=None, usegslrng=False):
+    """
+    Creates an I3Tray. Adds I3SummaryService context, I3RandomService context,
+        I3Reader module, and I3Writer module. Calls the passed configure_tray 
+        function for further tray configuration. Then, executes the tray, 
+        writes out summary to file, and prints stats/tray usage.
+
+    Args:
+        params
+            (dict) : parameters; forwarded to configure_tray function
+        configure_tray
+            (function) : the *function* called to further configure the tray; signature: (tray, params, stats, logger)
+        simulationname
+            (str) : the name of the simulation, used for logger
+        stats
+            (dict) : the stats dict
+        summaryfile
+            (str) : the filename to read/write the summary; if "", I3SummaryService is not added.
+        summaryin
+            (I3MapStringDouble) : the I3 summary inputted to I3SummaryService; if None, summary is read in from summaryfile 
+        doprintstats
+            (bool) : toggles whether to write out stats
+        inputfilenamelist
+            (list) : I3Reader module files; if None, doesn't add I3Reader
+        outputfile
+            (str) : I3Writer module file; if "", doesn't add I3Writer
+        outputskipkeys
+            (list) : I3Writer module skipkeys list
+        outputstreams
+            (list) : I3Writer streams; default OUTPUT_STREAMS
+        executionmaxcount
+            (int) : I3Tray exection max count; default None
+        seed
+            (int) : I3RandomService rng seed
+        nstreams
+            (int) : I3RandomService number of streams
+        streamnum
+            (int) : I3RandomService stream number
+        usegslrng
+            (bool) : toggles whether to use I3GSLRandomService or I3SPRNGRandomService for I3RandomService
+    """
+    logger = logging.getLogger(simulationname)
+    for param, value in sorted(params.items()):
+        logger.info("{0}:{1}".format(param, value))
+
+    # Set Up Tray
+    icetray.I3Logger.global_logger.set_level(icetray.I3LogLevel.LOG_DEBUG)
+    tray = I3Tray()
+
+    # Configure Tray
+    logger.info("Configuring IceProd: {0}".format(simulationname))
+    if summaryfile:
+        _add_i3_summary_service(tray, summaryfile, summaryin)
+    if inputfilenamelist:
+        tray.AddModule("I3Reader", "reader", filenamelist=inputfilenamelist)
+    if not seed is None:
+        _add_i3_random_service(tray, usegslrng, seed, nstreams, streamnum)
+    configure_tray(tray, params, stats, logger)
+    if outputfile:
+        _add_i3_writer(tray, outputfile, outputstreams, outputskipkeys)
+
+    # Execute Tray
+    logger.info("Executing {0}".format(simulationname))
+    #logger.info("\n{}".format(tray))
+    _execute(tray, executionmaxcount)
+
+    # Summarize
+    summaryout = tray.context['I3SummaryService']
+    _save_stats(tray, summaryout, stats)
+    if summaryfile:
+        WriteI3Summary(summaryout, summaryfile)
+
+    # Print Stats and Tray Usage
+    if doprintstats:
+        _print_stats(stats, logger, simulationname)
+        tray.PrintUsage()
+
+    # Free Memory
+    del tray
+
+    return summaryout
+
+
+# split up a comma-delimited string into list
+def _comma_list(arg_list, type):
+    return [type(a) for a in arg_list.split(',')]
+
+def int_comma_list(arg_list):
+    return _comma_list(arg_list, int)
+
+def float_comma_list(arg_list):
+    return _comma_list(arg_list, float)
+
+def str_comma_list(arg_list):
+    return _comma_list(arg_list, str)
+    
+
+# common arguments    
+def add_argument_gcdfile(parser):
+    parser.add_argument("--gcdfile", dest="gcdfile",
+                        default='', type=str, required=True,
+                        help='GeoCalibDetStatus filename')
+                        
+def add_argument_inputfile(parser):
+    parser.add_argument("--inputfile", dest="inputfile",
+                        default='', type=str, required=True,
+                        help='Input filename')
+               
+def add_argument_outputfile(parser):         
+    parser.add_argument("--outputfile", dest="outputfile",
+                        default='', type=str, required=True,
+                        help='Output filename')
+
+def add_argument_inputfilelist(parser):
+    parser.add_argument("--inputfilelist", dest="inputfilelist",
+                        default=[], type=str_comma_list, required=True,
+                        help='List of input filenames')
+
+def add_argument_nproc(parser):
+    parser.add_argument("--nproc", dest="nproc",
+                        default=1, type=int, required=False,
+                        help='Number of jobs (Number of RNG streams)')
+
+def add_argument_procnum(parser):
+    parser.add_argument("--procnum", dest="procnum",
+                        default=0, type=int, required=False,
+                        help='job number (RNG stream number)')
+
+def add_argument_seed(parser):
+    parser.add_argument("--seed", dest="seed",
+                        default=0, type=int, required=False,
+                        help='RNG seed')
+
+def add_argument_usegslrng(parser):
+    parser.add_argument("--UseGSLRNG", dest="usegslrng",
+                        default=False, action="store_true", required=False,
+                        help="Use I3GSLRandomService")
--- simprod-scripts/resources/scripts/clsim.py
+++ simprod-scripts/resources/scripts/clsim.py
@@ -1,12 +1,188 @@
 #!/usr/bin/env python
-import logging
-logging.basicConfig()
-rootLogger = logging.getLogger('')
-rootLogger.setLevel(logging.DEBUG)
 
-from icecube.simprod.modules import ClSim
+"""
+ GPU Photon propagation
+"""
 
-if __name__ == '__main__':
-   stats = {}
-   ptprop = ClSim()
-   ptprop.ExecuteOpts(stats)
+import os
+from os.path import expandvars
+from icecube.simprod.util import CombineHits, DrivingTime
+from icecube.simprod.util import ReadI3Summary, WriteI3Summary
+from icecube.simprod.util.fileutils import download,untar,isurl
+from icecube.simprod.util import simprodtray
+from icecube.simprod.util.simprodtray import RunI3Tray
+import argparse
+import json
+from icecube import clsim
+from icecube import polyplopia
+from icecube.production_histograms import ProductionHistogramModule
+from icecube.production_histograms.histogram_modules.simulation.mcpe_module import I3MCPEModule
+from icecube.simprod import segments
+from icecube import icetray, dataclasses, dataio, phys_services, interfaces
+from I3Tray import I3Tray, I3Units
+
+
+def add_args(parser):
+    """
+    Args:
+        parser (argparse.ArgumentParser): the command-line parser
+    """
+    simprodtray.add_argument_gcdfile(parser)
+    simprodtray.add_argument_inputfilelist(parser)
+    simprodtray.add_argument_outputfile(parser)
+                        
+    simprodtray.add_argument_nproc(parser)
+    simprodtray.add_argument_procnum(parser)
+    simprodtray.add_argument_seed(parser)  
+    simprodtray.add_argument_usegslrng(parser)  
+    
+    parser.add_argument("--summaryfile", dest="summaryfile",
+                        default='summary.json', type=str, required=False,
+                        help='JSON Summary filename')
+    parser.add_argument("--GPU", dest="gpu",
+                        default=None, type=str, required=False,
+                        help="Graphics Processing Unit number (shoud default to environment if None)")
+    parser.add_argument("--UseGPUs", dest="usegpus",
+                        default=False, action="store_true", required=False,
+                        help="Use Graphics Processing Unit")
+    parser.add_argument("--no-RunMPHitFilter", dest="runmphitfilter",
+                        default=True, action="store_false", required=False,
+                        help="Run polyplopia's mphitfilter")
+    parser.add_argument("--no-PropagateMuons", dest="propagatemuons",
+                        default=True, action="store_false", required=False,
+                        help='Run PROPOSAL to do in-ice propagation')
+    parser.add_argument("--PROPOSALParams", dest="proposalparams",
+                        default=dict(), type=json.loads, required=False,
+                        help='any other parameters for proposal')
+    parser.add_argument("--oversize", dest="oversize",
+                        default=5, type=int, required=False,
+                        help="over-R: DOM radius oversize scaling factor")
+    parser.add_argument("--holeiceparametrization", dest="holeiceparametrization",
+                        default=expandvars("$I3_SRC/ice-models/resources/models/angsens/as.h2-50cm"),
+                        type=str, required=False,
+                        help="Location of hole ice param files")
+    parser.add_argument("--efficiency", dest="efficiency",
+                        default=[1.00], type=simprodtray.float_comma_list, required=False,
+                        help="overall DOM efficiency correction")
+    parser.add_argument("--IceModelLocation", dest="icemodellocation",
+                        default=expandvars("$I3_BUILD/ice-models/resources/models"),
+                        type=str, required=False,
+                        help="Location of ice model param files")
+    parser.add_argument("--IceModel", dest="icemodel",
+                        default="spice_3.2", type=str, required=False,
+                        help="ice model subdirectory")
+    parser.add_argument("--MCTreeName", dest="mctreename",
+                        default="I3MCTree", type=str, required=False,
+                        help="Name of MCTree frame object")
+    parser.add_argument("--PhotonSeriesName", dest="photonseriesname",
+                        default="I3MCPESeriesMap", type=str, required=False,
+                        help="Photon Series Name")
+    parser.add_argument("--RawPhotonSeriesName", dest="rawphotonseriesname",
+                        default=None, type=str, required=False,
+                        help="Raw Photon Series Name")
+    parser.add_argument("--UseGeant4", dest="usegeant4",
+                        default=False, action="store_true", required=False,
+                        help="Enable Geant4 propagation")
+    parser.add_argument("--HistogramFilename", dest="histogramfilename",
+                        default=None, type=str, required=False,
+                        help='Histogram filename.')
+    parser.add_argument("--EnableHistogram", dest="enablehistogram",
+                        default=False, action="store_true", required=False,
+                        help='Write a SanityChecker histogram file.')
+    parser.add_argument("--no-KeepMCTree", dest="keepmctree",
+                        default=True, action="store_false", required=False,
+                        help='Delete propagated MCTree otherwise')
+
+
+def configure_tray(tray, params, stats, logger):
+    """
+    Configures the I3Tray instance: adds modules, segments, services, etc.
+
+    Args:
+        tray (I3Tray): the IceProd tray instance
+        params (dict): command-line arguments (and default values)
+                            referenced as dict entries; see add_args()
+        stats (dict): dictionary that collects run-time stats
+        logger (logging.Logger): the logger for this script
+    """
+    if params['gpu'] is not None and params['usegpus']:
+        os.putenv("CUDA_VISIBLE_DEVICES", str(params['gpu']))
+        os.putenv("COMPUTE", ":0." + str(params['gpu']))
+        os.putenv("GPU_DEVICE_ORDINAL", str(params['gpu']))
+
+    if len(params['efficiency']) == 1:
+        efficiency = params['efficiency'][0]
+    elif len(params['efficiency']) > 1:
+        efficiency = params['efficiency']
+    else:
+        raise Exception("Configured empty efficiency list")
+
+    if params['propagatemuons']:
+        if params['usegslrng']:
+           randomServiceForPropagators = phys_services.I3GSLRandomService(seed=2 * (params['seed'] * params['nproc'] + params['procnum']))
+        else:
+           randomServiceForPropagators = phys_services.I3SPRNGRandomService(seed=params['seed'], nstreams=params['nproc'] * 2, streamnum=params['nproc'] + params['procnum'])
+        tray.context['I3PropagatorRandomService'] = randomServiceForPropagators
+
+        tray.AddModule("Rename", "rename_corsika_mctree",
+                       Keys=[params['mctreename'], params['mctreename'] + '_preMuonProp'])
+        tray.AddSegment(segments.PropagateMuons, 'propagator',
+                        RandomService=randomServiceForPropagators,
+                        **params['proposalparams'])
+
+    tray.AddSegment(clsim.I3CLSimMakeHits, "makeCLSimHits",
+                    GCDFile=params['gcdfile'],
+                    RandomService=tray.context['I3RandomService'],
+                    UseGPUs=params['usegpus'],
+                    UseCPUs=not params['usegpus'],
+                    IceModelLocation=os.path.join(params['icemodellocation'], params['icemodel']),
+                    UnshadowedFraction=efficiency,
+                    UseGeant4=params['usegeant4'],
+                    DOMOversizeFactor=params['oversize'],
+                    MCTreeName=params['mctreename'],
+                    MCPESeriesName=params['photonseriesname'],
+                    PhotonSeriesName=params['rawphotonseriesname'],
+                    HoleIceParameterization=params['holeiceparametrization'])
+
+    if params['runmphitfilter']:
+        tray.AddModule("MPHitFilter", "hitfilter",
+                       HitOMThreshold=1,
+                       RemoveBackgroundOnly=False,
+                       I3MCPESeriesMapName=params['photonseriesname'])
+
+    if params['enablehistogram'] and params['histogramfilename']:
+        tray.AddModule(ProductionHistogramModule,
+                       Histograms=[I3MCPEModule],
+                       OutputFilename=params['histogramfilename'])
+
+    if not params['keepmctree']:
+        logger.info("discarding %s" % (params['photonseriesname']))
+        tray.Add("Delete", "clean_mctruth",
+                 Keys=[params['mctreename'], params['mctreename'] + '_preSampling'])
+
+
+def main():
+    """
+     GPU Photon propagation
+    """
+    # Get Params
+    parser = argparse.ArgumentParser(description="ClSim script")
+    add_args(parser)
+    params = vars(parser.parse_args())  # dict()
+
+    # Process Params
+    inputfilenamelist = [params['gcdfile']] + params['inputfilelist']
+    
+    # Execute Tray
+    summary = RunI3Tray(params, configure_tray, "ClSim",
+                        summaryfile=params['summaryfile'],
+                        inputfilenamelist=inputfilenamelist,
+                        outputfile=params['outputfile'],
+                        seed=params['seed'],
+                        nstreams=params['nproc'],
+                        streamnum=params['procnum'],
+                        usegslrng=params['usegslrng'])
+
+
+if __name__ == "__main__":
+    main()
