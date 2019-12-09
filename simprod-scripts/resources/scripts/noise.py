35a1c159dc54350ad6ba385abc7c44fbf30e606a
Eric Evans <eevans@icecube.wisc.edu>
2019-12-09 20:57:30 +0000

    modernize noise script
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@177794 16731396-06f5-0310-8873-f7f720988828

--- simprod-scripts/resources/scripts/noise.py
+++ simprod-scripts/resources/scripts/noise.py
@@ -1,13 +1,76 @@
-#!/bin/env python
-import logging
-logging.basicConfig()
-rootLogger = logging.getLogger('')
-rootLogger.setLevel(logging.INFO)
+#!/usr/bin/env python
 
-from icecube.simprod.modules import NoiseTriggers
+import os
+from icecube.simprod.segments import ProduceNoiseTriggers
+from icecube.simprod.util import ReadI3Summary, WriteI3Summary
+from icecube.simprod.util import simprodtray
+from icecube.simprod.util.simprodtray import RunI3Tray
+import argparse
+from icecube.simprod.util import BasicCounter, DAQCounter
+from icecube import icetray, phys_services, dataclasses
 
-if __name__ == '__main__':
-   stats = {}
-   det = NoiseTriggers()
-   det.ExecuteOpts(stats)
-   print(stats)
+
+def add_args(parser):
+    """
+    Args:
+        parser (argparse.ArgumentParser): the command-line parser
+    """
+    simprodtray.add_argument_gcdfile(parser)
+    simprodtray.add_argument_outputfile(parser)
+    
+    parser.add_argument("--summaryfile", dest="summaryfile",
+                        default='summary.json', type=str, required=False,
+                        help='JSON Summary filename')
+    parser.add_argument("--Detector", dest="detector",
+                        default="IC86:2011", type=str, required=False,
+                        help="detector to simulate")
+    parser.add_argument("--nevents", dest="nevents",
+                        default=1000, type=int, required=False,
+                        help='Number of events')
+    parser.add_argument("--RunId", dest="runid",
+                        default=0, type=int, required=False,
+                        help="Configure run ID")
+    parser.add_argument("--RNGSeed", dest="rngseed",
+                        default=0, type=int, required=False,
+                        help="RNG seed")
+
+
+def configure_tray(tray, params, stats, logger):
+    """
+    Configures the I3Tray instance: adds modules, segments, services, etc.
+
+    Args:
+        tray (I3Tray): the IceProd tray instance
+        params (dict): command-line arguments (and default values)
+                            referenced as dict entries; see add_args()
+        stats (dict): dictionary that collects run-time stats
+        logger (logging.Logger): the logger for this script
+    """
+    tray.AddSegment(ProduceNoiseTriggers, "noise_triggers",
+                    gcd_file=params['gcdfile'],
+                    nevents=params['nevents'],
+                    run_id=params['runid'])
+
+    tray.AddModule(BasicCounter, "count_triggers",
+                   Streams=[icetray.I3Frame.DAQ],
+                   name="%s Triggered Events" % params['detector'],
+                   Stats=stats)
+
+
+def main():
+    # Get Params
+    parser = argparse.ArgumentParser(description="NoiseTriggers script")
+    add_args(parser)
+    params = vars(parser.parse_args())  # dict()
+
+    # Execute Tray
+    summary = RunI3Tray(params, configure_tray, "NoiseTriggers",
+                        summaryfile=params['summaryfile'],
+                        outputfile=params['outputfile'],
+                        outputskipkeys=["I3Triggers", "MCPMTResponseMap", "MCTimeIncEventID"],
+                        executionmaxcount=4 + params['nevents'],
+                        seed=params['rngseed'])
+
+
+if __name__ == "__main__":
+    main()
