440835923dc69766f5a4e7b176831a58a828e02a
Alex Olivas <olivas@icecube.umd.edu>
2019-12-16 15:42:47 +0000

    massaged style of script, but no functional change.
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@177966 16731396-06f5-0310-8873-f7f720988828

--- sim-services/resources/scripts/SplitMCTreeForHybridSimulation.py
+++ sim-services/resources/scripts/SplitMCTreeForHybridSimulation.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 
-from I3Tray import *
-from icecube import icetray, dataio, sim_services
 import sys
+from I3Tray import I3Tray
+from icecube import icetray
+from icecube import dataio
+from icecube import sim_services
 
 if len(sys.argv) != 3: 
     print("usage: SplitMCTreeForHybridSimulation.py input_file output_file")
     sys.exit(-1)
 
 tray = I3Tray()
-tray.AddModule('I3Reader', 'reader', Filename=sys.argv[1])
-tray.AddModule('I3MCTreeHybridSimulationSplitter', 'treesplitter')
-tray.AddModule('I3Writer', 'writer', Filename=sys.argv[2])
-
-
+tray.AddModule('I3Reader', Filename=sys.argv[1])
+tray.AddModule('I3MCTreeHybridSimulationSplitter')
+tray.AddModule('I3Writer', Filename=sys.argv[2])
 tray.Execute()
 
