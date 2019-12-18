bece8207e31b3c2cba3135a27f08a4ffc6495275
Jakob van Santen <jvansanten@icecube.wisc.edu>
2019-12-18 19:13:50 +0000

    Add idiomatic cable position reader
    See #2388
    
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@178085 16731396-06f5-0310-8873-f7f720988828

---
+++ clsim/python/GetIceCubeCableShadow.py
@@ -0,0 +1,30 @@
+
+from icecube.icetray import OMKey
+from icecube.simclasses import I3CylinderMap, I3ExtraGeometryItemCylinder
+from icecube.dataclasses import I3Position
+
+from I3Tray import I3Units
+
+import numpy, math
+from os.path import expandvars
+
+def GetIceCubeCableShadow(CableAngles=expandvars("$I3_BUILD/ppc/resources/ice/dx.dat"),
+    DOMRadius=165.1*I3Units.mm, CableRadius=23*I3Units.mm, CableLength=1*I3Units.m):
+    """
+    Get a cylinder representing the position of the cable at each DOM
+    
+    :param CableAngles: text file containing string, om, angle (degrees), angle error (degrees)
+    :param DOMRadius: radius of the DOM sphere
+    :param CableRadius: radius of the cable
+    :param CableLength: length of the cable segment at each DOM
+    :returns: a map of I3ExtraGeometryItem representing the local position of
+        the cable *in DOM-centered coordinates*
+    """
+    # assume the cable runs along the surface of the DOM
+    radius = DOMRadius + CableRadius
+    shadows = I3CylinderMap()
+    for string, om, angle, _ in numpy.loadtxt(CableAngles, dtype=[('string',int),('om',int),('angle',float),('angle_err',float)]):
+        pos = dataclasses.I3Position(radius*np.cos(np.radians(angle)), radius*np.sin(np.radians(angle)), 0)
+        shadows[OMKey(string,om)] = I3ExtraGeometryItemCylinder(pos + I3Position(0,0,CableLength/2.), pos + I3Position(0,0,-CableLength/2.), CableRadius)
+
+    return shadows
