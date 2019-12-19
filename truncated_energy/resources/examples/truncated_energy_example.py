61643d8d3de0c925df325f843adaf53df8634707
Yang Lyu <yanglyu@lbl.gov>
2019-12-19 05:42:15 +0000

    my first commit! try if it works
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@178121 16731396-06f5-0310-8873-f7f720988828

--- truncated_energy/resources/examples/truncated_energy_example.py
+++ truncated_energy/resources/examples/truncated_energy_example.py
@@ -7,9 +7,10 @@ from icecube import icetray, dataclasses, simclasses
 load( "libtruncated_energy" )
 
 #   Input files
-testdir = os.environ["I3_TESTDATA"]
-files = [ os.path.join(testdir, 'GCD', 'GeoCalibDetectorStatus_2012.56063_V0.i3.gz'),
-          os.path.join(testdir, 'sim', "Level2_IC86.2011_corsika.010281.001664.00.i3.bz2")]
+gcdfile = ["/cvmfs/icecube.opensciencegrid.org/standard/RHEL_6_x86_64/i3ports/test-data/sim/GeoCalibDetectorStatus_2013.56429_V1.i3.gz"]
+infile  = ["/cvmfs/icecube.opensciencegrid.org/standard/RHEL_6_x86_64/i3ports/test-data/sim/Level2_IC86.2011_corsika.010281.001664.00.i3.bz2"]
+
+files = gcdfile + infile
 
 tray = I3Tray( )
 
@@ -71,6 +72,7 @@ keep_keys = [   "SPEFit2TruncatedEnergyTest_SPICEMie_AllBINS_MuEres",
                 "SPEFit2TruncatedEnergyTest_SPICEMie_BINS_Muon",
                 "SPEFit2TruncatedEnergyTest_SPICEMie_BINS_Neutrino",
                 "SPEFit2TruncatedEnergyTest_SPICEMie_BINS_dEdX",
+		"SPEFit2TruncatedEnergyTest_SPICEMie_BINS_dEdxVector",
                 "SPEFit2TruncatedEnergyTest_SPICEMie_DOMS_MuEres",
                 "SPEFit2TruncatedEnergyTest_SPICEMie_DOMS_Muon",
                 "SPEFit2TruncatedEnergyTest_SPICEMie_DOMS_Neutrino",
