facac0652ff3b95e4dc99bb017baa4b5a151f38a
Zelong Zhang <zelong.zhang.1@stonybrook.edu>
2019-12-20 00:56:49 +0000

    To fit with python3
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@178183 16731396-06f5-0310-8873-f7f720988828

--- level3-filter-cascade/python/level3_Globals.py
+++ level3-filter-cascade/python/level3_Globals.py
@@ -4,7 +4,7 @@ from icecube import icetray
 def which_split(frame,split_name=None):
     if frame.Stop == icetray.I3Frame.Physics:
         if frame['I3EventHeader'].sub_event_stream == split_name:
-#            print '------------InIce Split = ',frame['I3EventHeader'].sub_event_stream
+#            print ('------------InIce Split = ',frame['I3EventHeader'].sub_event_stream)
             return True
         else:
             return False
@@ -14,16 +14,16 @@ def which_split(frame,split_name=None):
 ## Here probably need to check with in_ice split    
 def label(frame,year):
     if year == "2012":
-	filter_name = 'CascadeFilter_12'
+        filter_name = 'CascadeFilter_12'
     elif year == "2011":
-	filter_name = 'CascadeFilter_11'
+        filter_name = 'CascadeFilter_11'
     else:
-	filter_name = 'CascadeFilter_13'
+        filter_name = 'CascadeFilter_13'
 
     if frame.Has('FilterMask'):
-	
+        
         if frame['FilterMask'][filter_name].condition_passed==1: # setting for 2015 data
-	#if frame['FilterMask']['CascadeFilter_12'].condition_passed==1: # for test purpose
+        #if frame['FilterMask']['CascadeFilter_12'].condition_passed==1: # for test purpose
             frame['CscdL2'] = icetray.I3Bool(True)
         else:
             frame['CscdL2'] = icetray.I3Bool(False)
@@ -32,4 +32,4 @@ def label(frame,year):
         frame['CscdL2'] = icetray.I3Bool(False)
 
 
-	
+        
