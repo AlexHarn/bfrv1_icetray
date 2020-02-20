2fa5666fabe7c63b2cfdddca86e31ce6c337277c
Juan Carlos Díaz Vélez <juancarlos@icecube.wisc.edu>
2020-02-20 20:13:07 +0000

    Added option to not propagate background
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@179014 16731396-06f5-0310-8873-f7f720988828

--- simprod-scripts/resources/examples/repoly.py
+++ simprod-scripts/resources/examples/repoly.py
@@ -12,12 +12,14 @@ parser.add_argument("--backgroundfile", type=str,
                    help="Pre-generated background CORSIKA events")
 parser.add_argument("--summaryfile", type=str, default=None,
                     help="(Optional) name of summary file. If not specified, no summary file will be made.")
-
+parser.add_argument("--propagate-background", dest="propagate",
+                        default=False, action="store_true", required=False,
+                   help="Propagate background events")
 parser.add_argument("--mctype", type=str, default="LeptonInjector_numu",
                    help="Primary Type (corsika, nugen, LeptonInjector, etc). To avoid overcounting with corsika.")
 parser.add_argument("--MCTreeName", type=str, default="I3MCTree", dest="mctree_name",
                    help="Name of I3MCTree objects to merge")
-parser.add_argument("--SeparateMCTreeName", type=str, default="", dest="separate_mctree_name",
+parser.add_argument("--BGTreeName", type=str, default="BackgroundI3MCTree_preMuonProp", dest="bgtree_name",
                    help="Put coincident events into this separate MCTree. Leave empty to combine.")
 parser.add_argument("--TimeWindow", type=float, default=40.0, dest="time_window",
                    help="Coincident event time window in *microseconds*.")
@@ -43,12 +45,11 @@ from icecube.simprod import segments
 infile      = args.infile
 outfile     = args.outfile
 RNGSeed     = args.seed
-bgfile      = args.backgroundfile
 SummaryFile = args.summaryfile
 
 MCtype             = args.mctype
 MCTreeName         = args.mctree_name
-SeparateMCTreeName = args.separate_mctree_name
+BGTreeName         = args.bgtree_name
 TimeWindow         = args.time_window * I3Units.microsecond
 Rate               = args.rate
 
@@ -65,7 +66,6 @@ tray.context['I3SummaryService'] = dataclasses.I3MapStringDouble()
 
 # Read pre-generated CORSIKA file with CR showers. Assuming the events follow natural spectrum (e.g. Hoerandel)
 # Used to inject background CR events from
-#background.open(bgfile)
 
 
 # Configure tray
@@ -89,30 +89,30 @@ tray.Add(check_if_neut_inice,"check_inice",
 tray.AddSegment(segments.PolyplopiaSegment,"merge",
                mctype = MCtype,
                mctree_name = MCTreeName,
-               bgfile = bgfile,
-               separate_coincident_mctree_name = "BackgroundI3MCTree_preMuonProp",
+               bgfile = args.backgroundfile,
+               separate_coincident_mctree_name = BGTreeName,
                timewindow = TimeWindow,
                rate = Rate*I3Units.hertz,
                RandomService = randomService)
 
-tray.AddModule("Rename","rename_mctree", Keys=['I3MCTree','SignalI3MCTree'])
+tray.AddModule("Rename","rename_mctree", Keys=[MCTreeName,'SignalI3MCTree'])
 tray.AddModule("Rename","rename_mmc", Keys=['MMCTrackList','SignalMMCTrackList'])
 
-tray.AddSegment(segments.PropagateMuons, 'propagator',
+if args.propagate:
+    tray.AddSegment(segments.PropagateMuons, 'propagator',
                RandomService= randomService,
                SaveState=False,
-               InputMCTreeName="BackgroundI3MCTree_preMuonProp",
+               InputMCTreeName=BGTreeName,
                OutputMCTreeName="BackgroundI3MCTree",
 #               **PROPOSALParams
 ) 
 
-def CombineSignal(frame):
+def CombineSignal(frame,signal_name='SignalI3MCTree',bg_name='BackgroundI3MCTree'):
 
-    signal_tree = frame['SignalI3MCTree']
-    combined_tree = frame['BackgroundI3MCTree']
+    signal_tree = frame[signal_name]
+    combined_tree = dataclasses.I3MCTree(frame[bg_name])
     polyplopia.MergeMCTrees(combined_tree, signal_tree,0)
     frame["I3MCTree"] = combined_tree
-    del frame['BackgroundI3MCTree']
 
     signal_mmc = None
     if 'MMCTrackList' in frame and 'SignalMMCTrackList' in frame:
