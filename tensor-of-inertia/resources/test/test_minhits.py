5940eebcd0dfc006cbc9aad9c78080a6935a6417
Andres Medina <andres.medina@icecube.wisc.edu>
2020-02-26 21:44:59 +0000

    added tests to improve line coverage
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@179136 16731396-06f5-0310-8873-f7f720988828

---
+++ tensor-of-inertia/resources/test/test_minhits.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os.path
+from I3Tray import I3Tray
+from icecube import dataclasses,tensor_of_inertia
+
+def check(frame):
+    assert(frame['ti'].fit_status == dataclasses.I3Particle.InsufficientHits)
+
+tray = I3Tray()
+
+I3_TESTDATA=os.environ['I3_TESTDATA']
+gcd_file = os.path.join(I3_TESTDATA,"GCD","GeoCalibDetectorStatus_2012.56063_V0.i3.gz")
+input_file = os.path.join(I3_TESTDATA,"sim","Level2_IC86.2011_corsika.010281.001664.00.i3.bz2")
+        
+tray.AddModule("I3Reader", FileNameList = [gcd_file,input_file] )
+
+tray.AddModule("I3TensorOfInertia",
+               InputReadout = "OfflinePulses",
+               MinHits=999999)
+
+tray.Add(check)
+
+tray.Execute(5)
+
+
+
+
