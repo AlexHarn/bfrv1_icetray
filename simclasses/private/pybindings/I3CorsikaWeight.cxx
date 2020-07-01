fd35369c9ad51cf16edea73bc4babfa04fe05eaa
Kevin Meagher <kmeagher@icecube.wisc.edu>
2020-07-01 16:19:09 +0000

    make these parameters read-write
    git-svn-id: http://code.icecube.wisc.edu/svn/meta-projects/combo/trunk@180846 16731396-06f5-0310-8873-f7f720988828

--- simclasses/private/pybindings/I3CorsikaWeight.cxx
+++ simclasses/private/pybindings/I3CorsikaWeight.cxx
@@ -10,10 +10,10 @@ void register_I3CorsikaWeight()
              bp::bases<I3FrameObject>,
              boost::shared_ptr<I3CorsikaWeight> >
     ("I3CorsikaWeight")
-    .def_readonly("primary",    &I3CorsikaWeight::primary)
-    .def_readonly("bias",       &I3CorsikaWeight::bias)
-    .def_readonly("weight",     &I3CorsikaWeight::weight)
-    .def_readonly("max_x",      &I3CorsikaWeight::max_x)
+    .def_readwrite("primary",    &I3CorsikaWeight::primary)
+    .def_readwrite("bias",       &I3CorsikaWeight::bias)
+    .def_readwrite("weight",     &I3CorsikaWeight::weight)
+    .def_readwrite("max_x",      &I3CorsikaWeight::max_x)
     .def(bp::dataclass_suite<I3CorsikaWeight>())
     ;
 }
